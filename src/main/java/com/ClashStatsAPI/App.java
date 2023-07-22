d355dc2289d54fadc4e72debaeb9da94c0353744
Loris Salvador <loris.salvador@student.hepl.be>
2023-07-22 07:39:10 +0200

    Initial commit

---
+++ src/main/java/com/ClashStatsAPI/App.java
@@ -0,0 +1,13 @@
+package com.ClashStatsAPI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
